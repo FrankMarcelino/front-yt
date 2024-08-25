946e55c2a26d963c8a15173130a6d6f1bf51ab2c
frankMarcelino <frank.silva.ti@gmail.com>
2024-08-25 16:42:10 -0300

    feet: add a biblioteca de icons do bootstraps para facilitar o desenvolvimento do menu e do restante do site

--- clone_yt/src/App.js
+++ clone_yt/src/App.js
@@ -1,6 +1,7 @@
 import Header from "./components/header";
 import Menu from "./components/menu";
 import { useState } from "react";
+import { House } from "react-bootstrap-icons";
 
 function App() {
   const [openMenu, setOpenMenu] = useState(true);
@@ -12,6 +13,7 @@ function App() {
         <Menu openMenu={openMenu} setOpenMenu={setOpenMenu} />
         <div style={{ backgroundColor: "#000", width: "100%" }}></div>
       </div>
+      <House size={40} />
     </div>
   );
 }
--- clone_yt/src/components/menu/index.tsx
+++ clone_yt/src/components/menu/index.tsx
@@ -1,21 +1,32 @@
 import { Container, MenuItem, ButtonIcon } from "./styles";
-import SearchIcon from "../../assets/search.png";
+import {
+  HouseDoor,
+  FileEarmarkPlay,
+  CollectionPlay,
+  PlayBtn,
+  ClockHistory,
+} from "react-bootstrap-icons";
 
 const itens = [
   {
     name: "Inicio",
+    icon: <HouseDoor size={20} />,
   },
   {
     name: "Shorts",
+    icon: <FileEarmarkPlay size={20} />,
   },
   {
     name: "Inscrições",
+    icon: <CollectionPlay size={20} />,
   },
   {
-    name: "Biblioteca",
+    name: "Você",
+    icon: <PlayBtn size={20} />,
   },
   {
     name: "Histórico",
+    icon: <ClockHistory size={20} />,
   },
 ]; // simulando uma requição para pegar os itens do menu
 
@@ -27,7 +38,7 @@ function Menu({ openMenu }: IProps) {
     <Container openMenu={openMenu}>
       {itens.map((item) => (
         <MenuItem openMenu={openMenu}>
-          <ButtonIcon alt="" src={SearchIcon} />
+          {item.icon}
           <span>{item.name}</span>
         </MenuItem>
       ))}
